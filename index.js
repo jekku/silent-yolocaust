ad52d75465235813f0a3926f3b3d55ac50c0e0fb
Jekri <jekri.orlina@any.tv>
2015-07-30 15:19:56 +0800

    Require UA code on initialize.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+'use strict';
+
 var React = require('react'),
+    GA = require('ga-tracking-code'),
+    _ = require('lodash');
 
 SilentYolocaust = React.createClass({
     displayName : 'Silent Yolocaust',
 
     render : function () {
+
+        if(_.has(this.props,'ua_tracking_code')){
+            GA.initialize();
+            ga('create',this.props.ua_tracking_code,'auto');
+            return false;
+        }
+
+        console.err("No UA tracking code has been provided");
         return false;
+
     }
 });
+
+module.exports = SilentYolocaust;
